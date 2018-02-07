a80429a1b0f731bdc328af45f781a533f57b9899
ValeOlivares <valentinaolivares85@gmail.com>
2018-02-07 15:20:27 -0300

    favicon y div responsive para dnd

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,13 @@ function enter () {
 	var user = document.getElementById('userLogin').value;
 	var pass = document.getElementById('passLogin').value;
 
-	if (user.length === 0) {
-		document.getElementById('userLogin').placeholder='Favor ingresar nombre de usuario';
-	} else if (pass.length < 6) {
+	if (user.length === 0 || pass.length === 0 || pass === '123456'){
+		document.getElementById('userLogin').placeholder = 'Favor ingresa un nombre de usuario';
 		document.getElementById('passLogin').value='';
-		document.getElementById('passLogin').placeholder='La contraseña debe tener al menos 6 caracteres';
-	} else if (pass === '123456') {
-		document.getElementById('passLogin').value='';
-	 	document.getElementById('passLogin').placeholder= 'Favor ingresar una contraseña más segura';
-	 } else {
-	 	location.href ="collage.html";
-	 }
+		document.getElementById('passLogin').placeholder = 'debe tener 6 caracteres'
+	} else {
+		location.href= 'collage.html'
+	}	
 }
 
 function allowDrop(ev) {
@@ -28,3 +24,10 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+
+function guardarDatos(){
+ localStorage.nombre = document.getElementById("user").value;
+ localStorage.password = document.getElementById("pass").value;
+}
+
